2312a64f9a834ba782a202a274a864b9018bbf4a
Ben Cooper <ben.f.cooper@gmail.com>
2021-11-16 21:32:04 +0000

    Added comments to all files

--- AI-Coursework/IDAstar.py
+++ AI-Coursework/IDAstar.py
@@ -2,19 +2,21 @@ import copy
 import itertools
 import time
 
+# Set a blank goal state and yield count
 goal_state = []
 yield_count = 0
 
 
+# Check if the current state is the goal state
 def is_goal(state):
-    # [y, x, [[0, 1, 2], [3, 4, 5], [6, 7, 8]]]
-    # Goal State
+    # Format : [y, x, [[0, 1, 2], [3, 4, 5], [6, 7, 8]]]
     if state == goal_state:
         return True
     else:
         return False
 
 
+# Function to determine which direction the tile can move
 def move_blank(i, j, n):
     if i + 1 < n:
         yield i + 1, j
@@ -26,14 +28,27 @@ def move_blank(i, j, n):
         yield i, j - 1
 
 
+# Function to perform the movement of the tile in the puzzle
 def move(state):
+    # Extract the information from the current state
     [i, j, grid] = state
+
+    # Get the dimensions of the game e.g 3x3 returns 3
     n = len(grid)
+
+    # Loop through possible moves
     for pos in move_blank(i, j, n):
         i1, j1 = pos
+
+        # Create a deep copy of the grid in order to allow a deep enough recursion
         grid = copy.deepcopy(grid)
+
+        # Swap the tiles places with the chosen move
         grid[i][j], grid[i1][j1] = grid[i1][j1], grid[i][j]
+
+        # Return the move
         yield [i1, j1, grid]
+
         grid[i][j], grid[i1][j1] = grid[i1][j1], grid[i][j]
 
 
@@ -63,18 +78,33 @@ def manhattan_distance(current_state, goal_state):
 
 
 def dfs_rec(path, bound):
+    # Define the yield count
     global yield_count
 
+    # Check if we have already found the solution
     if is_goal(path[-1]):
         return path
+
+    # Check if the manhattan distance between the current state and goal state are greater than the bound
     if manhattan_distance(path[-1], goal_state) > bound:
         return None
+
+    # Continue the recursion
     else:
+        # Get the next set of moves
         for next_state in move(path[-1]):
             if next_state not in path:
+
+                # Add the next state to the path
                 next_path = path + [next_state]
+
+                # Set the new bound
                 bound = manhattan_distance(next_path[-1], goal_state)
+
+                # Call the next recursion
                 solution = dfs_rec(next_path, bound)
+
+                # Recursion exit path
                 if solution is not None:
                     if yield_count > 0:
                         print("Yield count : " + str(yield_count))
@@ -84,7 +114,7 @@ def dfs_rec(path, bound):
 
 
 if __name__ == '__main__':
-    # Starting state
+    # Starting states
     start_states = [
         [0, 0, [[0, 1, 8], [3, 6, 7], [5, 4, 2]]],
         [2, 0, [[6, 4, 1], [7, 3, 2], [0, 5, 8]]],
@@ -97,19 +127,24 @@ if __name__ == '__main__':
     times = []
     sol_length = []
 
+    # Loop through the start states
     for state in start_states:
 
         start = time.time()
 
+        # Set the initial bound between the state and the goal state
         bound = manhattan_distance(state, goal_state)
 
+        # Start the recursion
         solution = dfs_rec([state], bound)
 
+        # Get the number of moves
         if solution is not None:
             print("Number of moves : " + str(len(solution) - 1))
 
         finish = time.time()
 
+        # Calculate the time spent
         total_time = finish - start
 
         print(total_time)
--- AI-Coursework/IDDFS.py
+++ AI-Coursework/IDDFS.py
@@ -47,9 +47,9 @@
 # 1558399 yields
 # Start state : [0, 2, [[5, 4, 0], [2, 3, 1], [8, 7, 6]]]
 # -------------
-# 20 moves
-# 7.15s
-# 561396 yields
+# 31 moves
+# 2853.51s
+# 221466957 yields
 # Start state : [2, 1, [[8, 6, 7], [2, 5, 4], [3, 0, 1]]]
 # -------------
 
@@ -57,19 +57,21 @@ import copy
 import itertools
 import time
 
+# Create a blank goal state and yield count
 goal_state = []
 yield_count = 0
 
 
+# Check to see if the current state matches the goal state
 def is_goal(state):
-    # [y, x, [[0, 1, 2], [3, 4, 5], [6, 7, 8]]]
-    # Goal State
+    # Format : [y, x, [[0, 1, 2], [3, 4, 5], [6, 7, 8]]]
     if state == goal_state:
         return True
     else:
         return False
 
 
+# Function to determine which direction the tile can move
 def move_blank(i, j, n):
     if i + 1 < n:
         yield i + 1, j
@@ -80,44 +82,78 @@ def move_blank(i, j, n):
     if j - 1 >= 0:
         yield i, j - 1
 
-    # if i + 1 >
 
-
+# Function to perform the movement of the tile in the puzzle
 def move(state):
+    # Define the yield count
     global yield_count
+
+    # Extract information from the state
     [i, j, grid] = state
+
+    # Get the dimensions of the game e.g 3x3 returns 3
     n = len(grid)
+
+    # Loop through possible moves
     for pos in move_blank(i, j, n):
         i1, j1 = pos
+
+        # Create a deep copy of the grid in order to allow a deep enough recursion
         grid = copy.deepcopy(grid)
+
+        # Swap the tiles places with the chosen move
         grid[i][j], grid[i1][j1] = grid[i1][j1], grid[i][j]
+
         yield_count += 1
+
+        # Return the move
         yield [i1, j1, grid]
+
         grid[i][j], grid[i1][j1] = grid[i1][j1], grid[i][j]
 
 
 def dfs_rec(path, depth):
+    # Define the yield count
     global yield_count
+
+    # Check if we have already found the solution
     if is_goal(path[-1]):
         return path
 
+    # Check to see if the recursion loop should be finished
     if depth == 0:
         return None
+
+    # Continue the recursion
     else:
+
+        # Collect the next state to analyse
         for next_state in move(path[-1]):
             if next_state not in path:
+
+                # Add the state to the path
                 next_path = path + [next_state]
+
+                # Run the next recursion at depth - 1
                 solution = dfs_rec(next_path, depth - 1)
+
+                # Check if the solution has been found
                 if solution is not None:
+
+                    # Add to the yield count
                     if yield_count > 0:
                         print("Yield count : " + str(yield_count))
                     yield_count = 0
+
+                    # Return any found solutions
                     return solution
 
     return None
 
 
 if __name__ == '__main__':
+
+    # Start states to try
     start_states = [
         [0, 0, [[0, 1, 8], [3, 6, 7], [5, 4, 2]]],
         [2, 0, [[6, 4, 1], [7, 3, 2], [0, 5, 8]]],
@@ -125,25 +161,32 @@ if __name__ == '__main__':
         [0, 2, [[5, 4, 0], [2, 3, 1], [8, 7, 6]]],
         [2, 1, [[8, 6, 7], [2, 5, 4], [3, 0, 1]]],
     ]
+
+    # Goal state to achieve
     goal_state = [2, 2, [[1, 2, 3], [4, 5, 6], [7, 8, 0]]]
     times = []
     sol_length = []
-    # Path = list of states[state, state]
 
+    # Loop through the given states
     for state in start_states:
         goal_state = goal_state
 
         start = time.time()
 
+        # Limit the depth depending on how many recursions have been done
         for depth_limit in itertools.count():
+
+            # The depth limit will slowly count upwards until recursion reaches 0
             solution = dfs_rec([state], depth_limit)
             if solution is not None:
+                # Get the number of moves
                 sol_length.append(len(solution))
                 print("Number of moves : " + str(len(solution) - 1))
                 break
 
         finish = time.time()
 
+        # Calculate the final time
         total_time = finish - start
 
         print("Time taken : " + str(total_time))
